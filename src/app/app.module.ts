0901c84645bd1f0d0b784792fdd6b75a69f92264
diego guzman <diego.guz.chivo@hotmail.es>
2019-09-27 09:43:48 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GuiasComponent } from './pages/guias/guias.component';
+import { GuiaComponent } from './pages/guia/guia.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import{FormsModule} from '@angular/forms'
+import{HttpClientModule} from '@angular/common/http'
+import { from } from 'rxjs';
+import { GuiasService } from './services/guias.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuiasComponent,
+    GuiaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    GuiasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
